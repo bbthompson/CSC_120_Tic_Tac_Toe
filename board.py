b903ebaac131fc58afc7057b3fa6a77423bae317
bbthompson <bbthompson@my.waketech.edu>
2022-04-27 20:34:03 -0400

    Stage 1 - Creates and Prints Board

---
+++ board.py
@@ -0,0 +1,10 @@
+#Create board (list of 3 lists)
+lstBoard = [["-","-","-"],["-","-","-"],["-","-","-"]]
+
+#Function to print board
+def printboard():
+    print("Printing board...")
+    for i in range(3):
+        print(lstBoard[i])
+
+printboard()
